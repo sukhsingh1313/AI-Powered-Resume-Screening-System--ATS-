55d4be715a2b0d4f697430f461643b3a8754ffd2
Sukhchain Singh <sukhsingh15666@gmail.com>
2026-04-05 21:33:31 +0530

    url patterns add

--- ats_project/urls.py
+++ ats_project/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path # re_path add kiya
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve # serve add kiya
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-# Yeh code media files (resumes) ko serve karne ke liye hai
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 👇 YEH NAYA CODE ADD KARNA HAI 👇
+# Yeh Django ko force karega ki Live server par bhi Media (PDF) files dikhaye
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
